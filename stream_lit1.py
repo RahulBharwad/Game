c7817edb030bbdaf41c64867340c7e22641f612b
RahulBharwad <123180092+RahulBharwad@users.noreply.github.com>
2023-05-26 10:46:13 +0530

    Add files via upload

---
+++ stream_lit1.py
@@ -0,0 +1,68 @@
+import random
+import streamlit as st
+
+def gamewin (comp, you):
+#If two values are equal, declare a tie!
+    if comp == you:
+        return None
+
+# Check for all possibilities when computer chose s
+    elif comp == "stone":
+        if you == "scissor":
+            return False 
+        elif you == "paper":
+            return True
+
+# Check for all possibilities when computer chose w 
+    elif comp == "paper":
+        if you == "stone":
+            return False 
+        elif you == "scissor":
+            return True
+
+# Check for all possibilities when computer chose g            
+    elif comp == "scissor":
+        if you == "paper":
+            return False 
+        elif you == "stone":
+            return True
+        
+def main():
+    st.title("STONE PAPER SCISSOR")
+
+    compturn = random.randint(1, 3)
+    if compturn == 1:
+        comp = "stone"
+    elif compturn == 2:
+        comp = "paper"
+    elif compturn == 3:
+        comp = "scissor"
+    else:
+        comp = None
+
+    your = st.form("Your turn: stone (st) paper (pa) scissor (sc)")
+    you= st.selectbox('your turn',('stone','paper','scissor'))
+    #if your == "st":
+        #you = "stone"
+    #elif your == "pa":
+        #you = "paper"
+    #elif your == "sc":
+        #you = "scissor"
+
+    st.write(f"Computer chose: {comp}")
+    st.write(f"YOU chose: {you}")
+
+    result = gamewin(comp, you)
+    if result == None:
+        st.write("TIE")
+    elif result == True:
+        st.write("YOU WON")
+    else:
+        st.write("SORRY, COMPUTER WON")
+    #elif you == "choose between stone, paper, scissor":
+        #st.write("Choose between stone, paper, scissor")
+
+
+if __name__ == "__main__":
+    main()
+
